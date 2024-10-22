7dd03a69ef3e46ef3db06ff0b9f39841754d7b58
istekhar <istekhararkam25@gmail.com>
2024-10-22 11:52:13 +0530

    first commit

---
+++ src/components/second-navbar.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { RiMenu2Line } from "react-icons/ri";
+import { IoAddOutline } from "react-icons/io5";
+
+const SecondNavbar = () => {
+  return (
+    <>
+      <div className="flex items-center justify-between w-full h-auto px-4 py-4 text-white bg-blue-500">
+        <div className="flex items-center gap-4 px-4 text-xl cursor-pointer">
+          <p>
+            <RiMenu2Line />
+          </p>
+          <div className="flex text-lg font-semibold capitalize gap-9">
+            <p>object manager</p>
+            <p>account</p>
+            <p>field & relationships</p>
+          </div>
+        </div>
+        <div className="mr-4 text-2xl bg-blue-600 cursor-pointer">
+          <IoAddOutline />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default SecondNavbar;
